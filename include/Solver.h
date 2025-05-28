12b6918692428303bbd88f37d8ddec82e33a6a4e
Anders P. Åsbø <anders.petershagen.asbo@outlook.com>
2025-05-28 21:42:10 +0200

    initial commit

---
+++ include/Solver.h
@@ -0,0 +1,16 @@
+//
+// Created by potteplante on 28.05.25.
+//
+
+#ifndef SOLVER_H
+#define SOLVER_H
+
+namespace DPLL {
+
+class Solver {
+
+};
+
+} // DPLL
+
+#endif //SOLVER_H
---
+++ src/Solver.cpp
@@ -0,0 +1,8 @@
+//
+// Created by potteplante on 28.05.25.
+//
+
+#include "../include/Solver.h"
+
+namespace DPLL {
+} // DPLL
